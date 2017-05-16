39292c26b4cc673229092a9d65261be5ab5bfb18
GuiBrandt <gui.g.brandt@gmail.com>
2017-05-16 13:20:03 -0300

    Não sei o que dizer

---
+++ Caminhos/Grafo.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Caminhos
+{
+    /// <summary>
+    /// Classe de grafo para as cidades
+    /// </summary>
+    class Grafo
+    {
+        /// <summary>
+        /// Estrutura para uma ligação entre cidades
+        /// </summary>
+        public class LigacaoCidades
+        {
+            int distancia;
+            int velocidadeMedia;
+
+            /// <summary>
+            /// Distância entre as duas cidades
+            /// </summary>
+            public int Distancia
+            {
+                get { return distancia; }
+                set
+                {
+                    if (value <= 0)
+                        throw new Exception("Distância inválida");
+                    distancia = value;
+                }
+            }
+
+            /// <summary>
+            /// Velocidade média do percurso
+            /// </summary>
+            public int VelocidadeMedia
+            {
+                get { return velocidadeMedia; }
+                set
+                {
+                    if (value < 0)
+                        throw new Exception("Velocidade inválida");
+                    velocidadeMedia = value;
+                }
+            }
+
+            /// <summary>
+            /// Construtor
+            /// </summary>
+            /// <param name="dist"></param>
+            /// <param name="vel"></param>
+            public LigacaoCidades(int dist, int vel)
+            {
+                Distancia = dist;
+                VelocidadeMedia = vel;
+            }
+        }
+    }
+}
